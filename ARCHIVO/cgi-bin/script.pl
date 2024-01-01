bf30eb3f8858a57268f060417c787cfc91e105f9
Christian Mestas Zegarra <cmestasz@unsa.edu.pe>
2024-01-01 18:32:20 -0500

    01/01/23

--- ARCHIVO/cgi-bin/script.pl
+++ ARCHIVO/cgi-bin/script.pl
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 use CGI;
+use Data::Dumper;
 
 my $cgi = CGI->new;
 my $name = $cgi->param("name");
@@ -15,10 +16,11 @@ my $idx = 0;
 my @results;
 open(IN, "<./programas2.csv");
 my @lines = <IN>;
-for (my $i = 0; $i < @lines; $i++) {
-    if ($lines[$i] =~ /(.*?)\|(.*?)\|(?:.*?\|){2}(.*?)\|(?:.*?\|){5}(.*?)\|(?:.*?\|){5}(.*?)\|(.*?)\|/) {
+for (my $i = 1; $i < @lines; $i++) {
+    if ($lines[$i] =~ /(.+?)\|(.+?)\|(?:.+?\|){2}(.+?)\|(?:.+?\|){5}(.+?)\|(?:.+?\|){5}(.+?)\|(.+?)\|/) {
         if ((!$name || $name eq $2) && (!$period || $period eq $3) && (!$local || $local eq $4) && (!$program || $program eq $5)) {
-            @results[$idx] = $1.": ".$2.". Licenciada por ".$3." años. Departamento: ".$4.". Programa: ".$5." - ".$6.".";
+            @results[$idx] = [$1, $2, $3." años", $4, $5, $6];
+            # $1.": ".$2.". Licenciada por ".$3." años. Departamento: ".$4.". Programa: ".$5." - ".$6.".";
             $idx++;
         }
     }
@@ -41,18 +43,33 @@ print<<BLOCK;
             <h1>Programas Encontrados</h1>
             <hr />
             <br />
-            <ul>
+            <table>
 BLOCK
 if (@results eq 0) {
     print "<h2>No se han encontrado programas de acuerdo a la búsqueda, revise si los filtros han sido ingresados correctamente.</h2>\n"
 } else {
-    for (my $i = 0; $i < @results; $i++) {
-        print "<li>".$results[$i]." </li>\n";
+    print<<BLOCK;
+                <tr>
+                    <th style="width: 75px">Código</th>
+                    <th style="width: 300px">Nombre</th>
+                    <th style="width: 75px">Periodo</th>
+                    <th style="width: 75px">Ciudad</th>
+                    <th style="width: 600px">Programa</th>
+                    <th style="width: 75px">Tipo</th>
+                </tr>
+BLOCK
+    foreach my $result (@results) {
+        print "<tr>\n";
+        foreach my $val (@$result) {
+            print "<td>".$val." </td>\n";
+        }
+        print "</tr>\n"
     }
 }
 print<<BLOCK;
-            </ul>
+            </table>
         </div>
+        <div style="height: 100px;"></div>
     </body>
 </html>
 BLOCK
